ca4d9bcfbc1ffbe60da07350f08a0ee0f4c7e347
Clinton Beasley <cbeasley92@gmail.com>
2016-12-12 07:23:28 +0100

    Less verbose in create script

--- create-rs.sh
+++ create-rs.sh
@@ -6,9 +6,7 @@ docker run -d -p 30001:27017 --name mongo01 --net mongo-cluster cbeasley92/mongo
 docker run -d -p 30002:27017 --name mongo02 --net mongo-cluster cbeasley92/mongodb:3.4 --replSet "rs0"
 docker run -d -p 30003:27017 --name mongo03 --net mongo-cluster cbeasley92/mongodb:3.4 --replSet "rs0"
 
-docker exec mongo01 mongo --eval "printjson(rs.initiate())"
-docker exec mongo01 mongo --eval "printjson(rs.conf())"
-docker exec mongo01 mongo --eval "printjson(rs.add(\"mongo02:27017\"))"
-docker exec mongo01 mongo --eval "printjson(rs.add(\"mongo03:27017\"))"
-docker exec mongo01 mongo --eval "printjson(rs.status())"
-
+docker exec mongo01 mongo --eval "rs.initiate()"
+docker exec mongo01 mongo --eval "rs.conf()"
+docker exec mongo01 mongo --eval "rs.add(\"mongo02:27017\")"
+docker exec mongo01 mongo --eval "rs.add(\"mongo03:27017\")"
